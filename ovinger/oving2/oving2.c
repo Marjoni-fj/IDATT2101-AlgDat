4214e916f50141f6d187062dc5f3307a508402f5
Fredrik <marjonifredrik@gmail.com>
2026-08-28 13:03:20 +0200

    feat[oving2]: Add method 2 according to the oving, ad test it out

--- ovinger/oving2/oving2.c
+++ ovinger/oving2/oving2.c
@@ -13,4 +13,18 @@ float method1(int n, float x) {
     }
 }
 
-int main() { printf("%f\n", method1(3, 2)); }
+float method2(int n, float x) {
+    if (n == 0 && x != 0) {
+        return 1;
+    }
+    if (n % 2 == 0) {
+        return method2(n / 2, x * x);
+    } else {
+        return x * method2((n - 1) / 2, x * x);
+    }
+}
+
+int main() {
+    printf("%f\n", method1(3, 2));
+    printf("%f\n", method2(3, 2));
+}
